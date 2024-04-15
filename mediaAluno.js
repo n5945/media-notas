370b99e3c6814805aaa21d5763e74e3f7fc27da6
n5945 <162457150+n5945@users.noreply.github.com>
2024-04-15 09:12:05 -0300

    Add files via upload

---
+++ mediaAluno.js
@@ -0,0 +1,18 @@
+const notas = [];
+notas.push(10, 7.5, 5, 8)
+let soma = 0
+let elementos 
+
+//=> Armazenar dentro de uma constante o total das notas do aluno
+const total = notas.reduce((acc, curr) = acc + curr)
+
+const total = notas.reduce((acc, curr) => acc + curr)
+for(let index = 0; index < array.length; index ++) {
+    const nota = notas[index] ; 
+    soma = nota + soma
+}
+
+//=> Calcular a média das notas do aluno
+//=> Retornar o resultado da média
+
+
